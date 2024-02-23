79c8727e0c0fd5191a16a773b93d308846d4fef5
Aneel Grewal <aneel.grewal@hotmail.com>
2024-02-24 00:50:45 +0100

    switch between player 1 and player 2

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -4,29 +4,41 @@ let playerOneScore = 0;
 let playerTwoScore = 0;
 let playerOnePoints = 0;
 let playerTwoPoints = 0;
+let activePlayer = "one";
 
 generateView();
+const newGameBtn = document.querySelector(".newGame");
+const rollDiceBtn = document.querySelector(".roll-dice");
+const holdBtn = document.querySelector(".hold");
+const diceEle = document.querySelector(".dice");
+const playerOne = document.querySelector(".player-1");
+const playerTwo = document.querySelector(".player-2");
+
+const player1Point = document.querySelector(".player-1-points");
+const player1Scores = document.querySelector(".player-1-score");
+const player2Point = document.querySelector(".player-2-points");
+const player2Scores = document.querySelector(".player-2-score");
 
 function generateView() {
   root.innerHTML =
     /*HTML*/
     `
     <div class="container">
-    <div class="player-1">
+    <div class="player-1 activePlayer">
       <h3>PLAYER 1</h3>
-      <span class="player-1-score">${playerOneScore}</span>
+      <span class="player-1-score">0</span>
       <div class="player-1-board">
         <span>CURRENT</span>
-        <span class="player-1-points">${playerOnePoints}</span>
+        <span class="player-1-points">0</span>
       </div>
     </div>
 
     <div class="player-2">
       <h3>PLAYER 2</h3>
-      <span class="player-2-score">${playerTwoScore}</span>
+      <span class="player-2-score">0</span>
       <div class="player-2-board">
         <span>CURRENT</span>
-        <span class="player-2-points">${playerTwoPoints}</span>
+        <span class="player-2-points">0</span>
       </div>
     </div>
     <div class="container2">
@@ -34,7 +46,7 @@ function generateView() {
      
       <div class="roll-hold">
       <div class="dice-container">
-      <img class="dice hidden" alt="image" src="./images/dice-1.png" />
+      <img class="dice hidden" alt="image" src="./images/dice-2.png" />
       </div>
         <div class="roll-dice btn">🎲 ROLL DICE</div>
         <div class="hold btn">💰 HOLD</div>
@@ -43,3 +55,42 @@ function generateView() {
   </div>
     `;
 }
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+rollDiceBtn.addEventListener("click", function () {
+  const diceNo = rollDice();
+  console.log(diceNo);
+  diceEle.classList.remove("hidden");
+  diceEle.src = `./images/dice-${diceNo}.png`;
+
+  //check for rolled = 1
+  if (diceNo !== 1) {
+    if (activePlayer === "one") {
+      playerOnePoints += diceNo;
+      console.log(playerOnePoints);
+      player1Point.textContent = playerOnePoints;
+    } else {
+      playerTwoPoints += diceNo;
+      console.log(playerTwoPoints);
+      player2Point.textContent = playerTwoPoints;
+    }
+  } else {
+    if (activePlayer === "one") {
+      playerOnePoints = 0;
+      player1Point.textContent = playerOnePoints;
+      playerOne.classList.remove("activePlayer");
+    } else {
+      playerTwoPoints = 0;
+      player2Point.textContent = playerTwoPoints;
+      playerTwo.classList.remove("activePlayer");
+    }
+
+    activePlayer = activePlayer === "one" ? "two" : "one";
+    activePlayer == "one"
+      ? playerOne.classList.add("activePlayer")
+      : playerTwo.classList.add("activePlayer");
+  }
+});
